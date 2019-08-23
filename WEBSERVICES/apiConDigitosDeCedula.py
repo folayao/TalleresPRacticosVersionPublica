635d65a78e6e62ef5557370c2d9633ca064115a8
Felipe Olaya <folayao@eafit.edu.co>
2019-08-23 11:40:12 -0500

    Todo listo. Att Felipe Olaya

--- WEBSERVICES/apiConDigitosDeCedula.py
+++ WEBSERVICES/apiConDigitosDeCedula.py
@@ -11,13 +11,13 @@ CORS(app)
 tipo_medicion= {'sensor' : 'HC-SR04','variable':'distancia','unidades':'centimetros'}
 
 mediciones = [
-    {'fecha': '2019-08-07 12:23:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-06 12:100:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-05 12:21:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-04 12:14:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-04 12:24:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-03 12:100:00',**tipo_medicion, 'valor': random.randint(0,100)},
-    {'fecha': '2019-08-03 12:20:00',**tipo_medicion, 'valor': random.randint(0,100)}
+    {'fecha': '2019-08-07 12:23:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-06 12:400:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-05 12:21:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-04 12:14:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-04 12:24:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-03 12:400:00',**tipo_medicion, 'valor': random.randint(0,400)},
+    {'fecha': '2019-08-03 12:20:00',**tipo_medicion, 'valor': random.randint(0,400)}
 ]
 
 #tipo de medicion
@@ -39,21 +39,30 @@ def getMedia():
 
 @app.route('/mediciones/mediana')#GET obtener info mediana
 def getMediana():
-    
     mediana= []
     for m in mediciones:
         mediana.append(m['valor'])
-
     return jsonify(stats.median(mediana))
     
 @app.route('/mediciones/getAll')#GETALL obtener info
-def getAllMedia():
+def getAll():
     return jsonify(mediciones)
 
-#@app.route('/mediciones/', methods=[POST])#POST es para crear nuevos archivos.             
-#def postMedia():
-    
+@app.route('/mediciones/post', methods=['POST']) #post es para crear nuevos archivos.             
+def post():
+    body = request.json
+    now = datetime.now()
+    body['fecha'] = datetime.strftime(now,'%Y-%m-%d %H:%M:%S')
+    mediciones.append({**body, **tipo_medicion})
+    return jsonify(mediciones)
 
-
+@app.route('/mediciones/delete<string:fecha>', methods=['DELETE'])
+def deleteOne(fecha):
+    for m in mediciones:
+        fechaDelete = m['fecha']
+        if fecha == fechaDelete:
+            fechaDelete.remove(fecha)
+        else:
+            print ("no se encuentra")
 app.run(port=5000, debug=True) #simepre tiene que ir al final para definir el puerto
 
